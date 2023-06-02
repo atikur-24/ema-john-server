9ecbe6a2bce7cbe74182511990b373db3084584c
Atikur <atikurrahman7223@gmail.com>
2023-06-02 12:32:24 +0600

    data load from DB

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -24,6 +24,31 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const database = client.db('emaJohnDB');
+    const productCollection = database.collection('products');
+
+    app.get('/products', async(req, res) => {
+      const page = parseInt(req.query.page) || 0;
+      const limit = parseInt(req.query.limit) || 10;
+      const skip = page * limit;
+      const result = await productCollection.find().skip(skip).limit(limit).toArray();
+      res.send(result);
+    })
+    
+    app.get('/totalProducts', async(req, res) => {
+      const result = await productCollection.estimatedDocumentCount();
+      res.send({ totalProducts: result });
+    })
+
+    app.post('/productsByIds', async(req, res) => {
+      const ids = req.body;
+      const objectIds = ids.map(id => new ObjectId(id))
+      const query = { _id: { $in: objectIds } };
+      const result = await productCollection.find(query).toArray();
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
